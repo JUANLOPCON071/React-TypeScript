8f51caca82387d99ac2c7e8801a62ded7f8bb5ff
juanlopcon07 <juan.21futmas07@gmail.com>
2025-03-31 21:38:24 -0500

    El objeto props y children

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { RandomFox } from "app/components/RandomFox";
 
+const random = () => Math.floor(Math.random()*123) + 1;
+
 export default function Home() {
   return (
     <div>
@@ -8,7 +10,7 @@ export default function Home() {
         <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1>
-        <RandomFox/>
+        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`}/>
       </main>
 
       <footer></footer>
--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,9 +1,8 @@
 import React, { JSX } from "react";
 
-const random = () => Math.floor(Math.random()*123) + 1;
+type Props = { image: string };
 
-export const RandomFox = (): JSX.Element => {
-    const image: string = `https://randomfox.ca/images/${random()}.jpg`;
+export const RandomFox = ({ image }: Props): JSX.Element => {
 
     return <img width={320} height="auto" src={image} className="rounded" />;
 };
