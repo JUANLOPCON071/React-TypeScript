840ad7ecaf54414a810a2b6a03e79c34f91441ba
juanlopcon07 <juan.21futmas07@gmail.com>
2025-04-02 19:29:08 -0500

    Tipos de referencias y observadores

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,8 +1,9 @@
-import React, { JSX } from "react";
+import React, { JSX, useRef } from "react";
 
 type Props = { image: string };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
+    const node = useRef<HTMLImageElement>(null);
 
-    return <img width={320} height="auto" src={image} className="rounded" />;
+    return <img ref={node} width={320} height="auto" src={image} className="rounded" />;
 };
