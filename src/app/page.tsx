49ae40471fca826ac9559a83e71b94a335655301
juanlopcon07 <juan.21futmas07@gmail.com>
2025-04-06 20:15:38 -0500

    Librerias no tipadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { LazyImage } from "app/components/RandomFox";
 import { MouseEventHandler, useState } from "react";
+import { random } from "lodash";
 
-const random = () => Math.floor(Math.random()*123) + 1;
+const myRandom = () => random(1, 123);
 
 const generateId = () => Math.random().toString(36).substr(2, 9);
 
@@ -12,7 +13,7 @@ export default function Home() {
   const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
     const newImageItem: IFoxImageItem = {
       id: generateId(),
-      url: `https://randomfox.ca/images/${random()}.jpg`,
+      url: `https://randomfox.ca/images/${myRandom()}.jpg`,
     };
 
     setImages([
